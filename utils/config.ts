4d50e1b7bb6e78c27a56be722b303819f3eb601b
Jatin Bansal <jatin@altruistindia.com>
2026-09-05 14:21:33 +0530

    whatsapp no

--- utils/config.ts
+++ utils/config.ts
@@ -1,8 +1,8 @@
 export const CONFIG = {
   brandName: "Reddy Anna",
   domainName: "reddyreal.live",
-  whatsappUrl: "https://wa.me/918796686330",
-  whatsappSupportUrl: "https://wa.me/918796686330",
+  whatsappUrl: "https://wa.me/918360750829",
+  whatsappSupportUrl: "https://wa.me/918360750829",
   
   logoUrl: "https://reddybookwinin.in/wp-content/uploads/2025/06/image-659e7b71ad644.webp",
   faviconUrl: "https://reddybookwinin.in/wp-content/uploads/2025/06/cropped-download-1-1-150x150.png",
